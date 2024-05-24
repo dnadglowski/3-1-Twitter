09db3b1ef3a8ec7cce500631a45798a1bcdff51d
dnadglowski <156368149+dnadglowski@users.noreply.github.com>
2024-05-24 21:38:09 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+   string input = Console.ReadLine();
+    
+    if (input.Length <= 140){
+      Console.WriteLine("Posted");
+      }
+    else{
+      Console.WriteLine("Rejected");
+      }
+    }
+  }
+ 
